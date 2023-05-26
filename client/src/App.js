6c68b9066d570937fa7420152919a563f1a31d47
ccvuong <116984891+ccvuong@users.noreply.github.com>
2023-05-25 21:37:22 -0700

    Updated front end files

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './App.css'; 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
 import Home from './pages/Home';
-import Matchup from './pages/Matchup';
-import Vote from './pages/Vote';
-import NotFound from './pages/NotFound';
+import Adopt from './pages/Adopt';
+import Contact from './pages/Contact';
+import Donate from './pages/Donate';
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -15,26 +20,30 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <div className="flex-column justify-center align-center min-100-vh bg-primary">
-          <Routes>
-            <Route 
-              path="/" 
-              element={<Home />}
-            />
-            <Route 
-              path="/matchup" 
-              element={<Matchup />}
-            />
-            <Route 
-              path="/matchup/:id" 
-              element={<Vote />}
-            />
-            <Route 
-              path="*"
-              element={<NotFound />}
-            />
-          </Routes>
-        </div>
+        <Navbar />
+
+
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/adopt"
+            element={<Adopt />}
+          />
+          <Route
+            path="/contact"
+            element={<Contact />}
+          />
+          <Route
+            path="/donate"
+            element={<Donate />}
+          />
+        </Routes>
+
+
+        <Footer />
       </Router>
     </ApolloProvider>
   );
