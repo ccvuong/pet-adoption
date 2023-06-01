aaa91072dde5b67596ea72b265f80a2d850e8faf
ccvuong <116984891+ccvuong@users.noreply.github.com>
2023-06-01 04:05:29 -0700

    mass update on home page!

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import "../styles/Footer.css";
+import { Link } from 'react-router-dom';
 
 // social media links
 import PetsIcon from '@mui/icons-material/Pets';
-import { Link } from '@mui/material';
 
 
 function Footer() {
@@ -12,10 +12,12 @@ function Footer() {
             <div className='socialMedia'>
                 <PetsIcon />
                 <p className='footer-links'>
-                    <Link to="/"> Home </Link>
-                    <Link to="/adopt"> Adopt </Link>
-                    <Link to="/contact"> Contact </Link>
-                    <Link to="/donate"> Donate </Link>
+
+                    <Link to="/" id='homeBtn'> Home </Link>
+                    <Link to="/adopt" id='adoptBtn'> Adopt </Link>
+                    <Link to="/contact" id='contactBtn'> Contact </Link>
+                    <Link to="/donate" id='donateBtn'> Donate </Link>
+
                 </p>
             </div>
 
--- client/src/pages/Adopt.js
+++ client/src/pages/Adopt.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Adopt.css';
+import AdoptItem from '../components/AdoptItem';
+
 
 function Adopt() {
   return (
-    <div>Adopt</div>
+    <div className='adoptPage'>
+
+      <h1 className='title'>Friends available for adoption nearby</h1>
+
+      <div className='petList'>
+      <AdoptItem></AdoptItem>
+
+      </div>
+    </div>
+
   )
 }
 
-export default Adopt
+export default Adopt;
