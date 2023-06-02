be6eef8842c8d00b3b54a3bfd418404627cc7711
ccvuong <116984891+ccvuong@users.noreply.github.com>
2023-06-02 03:28:53 -0700

    Updated adopt page files

--- client/src/components/AdoptItem.js
+++ client/src/components/AdoptItem.js
@@ -1,35 +1,50 @@
-import * as React from 'react';
+import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { Button, CardActionArea, CardActions } from '@mui/material';
 
-// pet images
-import BobaCat from '../assets/Boba.jpg';
-
-export default function petList() {
+export default function petList({ name, image, type }) {
   return (
     <div className='petListings'>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image={BobaCat}
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Boba
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Boba was surrendered to us by her owners. She is 3 years of age and loves to sleep on your lap.
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
 
+      <div className='pet'>
+        <Card>
+
+          <CardActionArea>
+
+            <CardMedia
+              component="img"
+              id='imageCard'
+              style={{ backgroundImage: `url("${image}")` }}
+
+            />
+
+            <CardContent id="content">
+
+              <Typography gutterBottom variant="h5" component="div">
+                <p className='name'>{name}</p>
+
+              </Typography>
+
+              <Typography variant="body2" color="text.secondary">
+                <p className='type'>{type}</p>
+
+              </Typography>
+
+            </CardContent>
+
+          </CardActionArea>
+
+          <CardActions>
+            <Button size="small" color="primary">
+              <p className='petBtn'>Check me out!</p>
+            </Button>
+          </CardActions>
+
+        </Card>
+      </div>
     </div>
   );
 }
---
+++ client/src/helpers/AdoptResults.js
@@ -0,0 +1,39 @@
+import BobaCat from '../assets/Boba.jpg';
+import Cosmos from '../assets/Cosmos.jpg';
+import Grogu from '../assets/Grogu.jpg';
+import Peanut from '../assets/Peanut.jpg';
+import Thor from '../assets/Thor.jpg';
+import Zeus from '../assets/Zeus.jpg';
+
+export const AdoptResults = [
+    {
+        name: "Boba",
+        image: BobaCat,
+        type: 'Boba was surrendered to us by her owners. She is 3 years of age and loves to sleep on your lap.'
+    },
+    {
+        name: "Cosmos",
+        image: Cosmos,
+        type: 'Cosmos is a new addition to our family! She is 4 years old and loves toys!'
+    },
+    {
+        name: "Grogu",
+        image: Grogu,
+        type: 'Grogu loves treats, especially peanut butter flavored ones. He is a vocalist when he wants attention.'
+    },
+    {
+        name: "Peanut",
+        image: Peanut,
+        type: 'Peanut will be your next adventure buddy. He will scout and protect from any danger, especially leaves!'
+    },
+    {
+        name: "Thor",
+        image: Thor,
+        type: 'Young Thor enjoys walks and sleeping on your lap. He is shy but very loving once he knows you.'
+    },
+    {
+        name: "Zeus",
+        image: Zeus,
+        type: 'Like his name, he is fast like lighting and will spawn anywhere around you!'
+    }
+];
